e3244b1d60d335587535727049629f079e543a08
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-14 16:23:28 +0530

    added note feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [note, setNote] = useState([{
+    title: "",
+    content: ""
+  }]);
+
+  const newNote = (title, content) => {
+    setNote(()=>{
+      return {
+        ...note,
+        title,
+        content,
+      }
+    })
+    console.log(note);
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea newNote={newNote} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-function CreateArea() {
+function CreateArea({newNote}) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const changeNote = (e) => {
+    e.preventDefault();
+    newNote(title, content);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" value={title} placeholder="Title" onChange={e=> setTitle(e.target.value)} />
+        <textarea name="content" value={content} onChange={e=> setContent(e.target.value)} placeholder="Take a note..." rows="3" />
+        <button onClick={changeNote}>Add</button>
       </form>
     </div>
   );
